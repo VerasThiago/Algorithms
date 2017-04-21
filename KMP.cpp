4868870979ef836be791a4d10a13888a46f3dcd0
Thiago Veras <thiagomachado@cjr.org.br>
2017-04-21 19:28:27 -0300

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -3,14 +3,17 @@ using namespace std;
 vector<int> strong_borders(const string& pat){
     int m = pat.size();
     vector<int> sbord(m);
-    int t = 0;
+    int t = -1;
+    sbord[0] = 0;
 
-    for (int j = 1; j < m; ++j){
-        while (t > 0 and pat[t] != pat[j])
-            t = sbord[t];
-        if(pat[t] == pat[j])
+    for (int j = 1; j < m; ++j){ 
+        while( (t > -1) and pat[t+1] != pat[j]){
+            t = sbord[t]-1;
+        }
+        if( pat[t+1] == pat[j])
             t++;
-        sbord[j] = t;
+       
+        sbord[j] = t+1;
     }
     return sbord;
 }
@@ -37,9 +40,33 @@ int KMP(const string& text, const string& pat)
     } 
     return occ;
 }
+int period(const string& a){
+    string aux = a;
+    aux += aux;
+    int m = aux.size();
+    int ans;
+    vector<int> v = strong_borders(aux);
+    for(int i = 0 ; i < aux.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
 
+
+    for(int i = 0 ; i < aux.size(); i++){
+        if(v[i] >= m/2){
+            printf("entrou\n");
+            ans = i - v[i] + 1;
+            return ans;
+        }
+    }
+
+
+}
 int main (){
     string a , b;
-    cin >> a >> b;
-    cout << KMP(a,b) << endl;
+    cin >> a;
+    vector<int> v=  strong_borders(a);
+    for(int i = 0 ; i < v.size(); i++)
+        cout << v[i] << " ";
+
+    //cout << "The period is " << period(a) << endl;
 }
