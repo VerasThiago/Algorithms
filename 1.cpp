edc98943df46db888fc72680faafacfa56a9c847
Thiago Veras <thiagomachado@cjr.org.br>
2017-04-21 22:29:51 -0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,74 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> strong_borders(const string& pat){
-    int m = pat.size();
-    vector<int> sbord(m);
-    int t = -1;
-    sbord[0] = 0;
 
-    for (int j = 1; j < m; ++j){ 
-        while( (t > -1) and pat[t+1] != pat[j]){
-            t = sbord[t]-1;
-        }
-        if( pat[t+1] == pat[j])
-            t++;
-       
-        sbord[j] = t+1;
-    }
-    return sbord;
+bool palindrome(string a){
+    int n = a.size();
+    for(int i = 0 ; i < n/2; i++)
+        if(a[i] != a[n-1-i])
+            return false;
+    return true;    
 }
 
-bool KMP(const string& text, const string& pat){
-    int n = text.size()-1;
-    int m = pat.size()-1;
-    int i = 0, j = 0, occ = 0, k = 0;
-
-    vector<int> bords = strong_borders(pat);
-
-    while (n - k >= m)
-    {
-        while (j <= m and text[i] == pat[j]){
-            ++j;
-            ++i;
-        }
-
-        if (j > m) return true;
-
-        if(j-1 >= 0 and bords[j-1] > 0)           
-                k = i - bords[j-1];
-   
-        else{
-            if(i == k) i++;
-               k = i;
-        }
-        
-        if(j > 0) j = bords[j-1];
-    }
-    return false;     
-}
 int main (){
-    int n , m, tam;
-    scanf("%d", &m);
-    
-    for(int j = 0 ; j < m ;j++){
-        bool ans = true;
-        scanf("%d", &tam);
-        
-        vector<string> v(tam);
-        for(int i = 0 ; i < tam ; i++)
-            cin >> v[i];       
-         
-        int k, l;
-        for( k = 0 ; k < tam-1 ; k++){
-            for( l = k+1; l < tam ; l++){
-                if(KMP(v[l],v[k])){
-                    ans = false;
-                    break;
+    string a;
+    while(cin >> a){
+        int x = a.size()-1; 
+        if(palindrome(a))
+            cout << a << endl;
+        else{
+            int big = 0;
+            string aux = "";
+            for(int i = a.size()-1; i >= 0; i--){
+                aux += a[i];
+                if(palindrome(aux)){
+                   // cout << aux << " EH PALINDROMO" << endl;
+                    int l = aux.size();
+                    big = max(big,l);
+                    //cout << "big vale " << big << endl;
                 }
             }
-            
+            cout << a;
+            for(int i = x - big; i >= 0 ; i--)
+                cout << a[i];
+            cout << endl;
         }
-        printf("%s\n",ans? "YES":"NO" );   
     }
-    
 }
