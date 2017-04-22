b67fec32404fcb769ce2c8518e9b806cb6a6cfd9
Thiago Veras <thiagomachado@cjr.org.br>
2017-04-21 21:44:23 -0300

    Update StringPeriod.cpp

--- StringPeriod.cpp
+++ StringPeriod.cpp
@@ -17,29 +17,7 @@ vector<int> strong_borders(const string& pat){
     }
     return sbord;
 }
-int KMP(const string& text, const string& pat)
-{
-    int n = text.size();
-    int m = pat.size();
-    int i = 0, j = 0, occ = 0;
 
-    vector<int> bords = strong_borders(pat);
-
-    while (i <= n - m)
-    {
-        while (j < m and pat[j] == text[i + j])
-            ++j;
-
-        if (j == m)
-            ++occ;
-
-        int KMP_shift = j - bords[j];
-
-        i += KMP_shift;
-        j = max(0, j - KMP_shift);
-    } 
-    return occ;
-}
 
 int period(const string& a){
     string aux = a;
