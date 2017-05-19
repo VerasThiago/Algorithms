b6ff8a1fa05166289f1d996fad1fd56b5c820b1e
Thiago Veras <thiagomachado@cjr.org.br>
2017-05-19 10:01:18 -0300

    Create aux.cpp

---
+++ Data Structures/aux.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std/
+
+#define ii pair <int,int>
+
+int main(){
+    int n, x;
+    cin >> n;
+    map<int,int> v1,v2,v3;
+    vector<int> preco(n);
+    vector<ii> num(n);
+  
+    for(int i = 0 ; i < n ; i++) cin >> preco[i];
+    for(int i = 0 ; i < n ; i++) cin >> num[i].first;
+    for(int i = 0 ; i < n ; i++) cin >> num[i].second;
+    
+    for(int i = 0 ; i < n ; i++){
+        
+    }
+    int cliente;
+    cin >> cliente;
+    for(int i = 0 ; i < cliente ; i++){
+        cin >> x;
+        
+    }
+        
+        http://codeforces.com/contest/799/problem/B
+        http://codeforces.com/blog/entry/51947
+
+    
+    
+}
