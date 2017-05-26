73b9995fb654ddad8d2d1ef4511764e4d0b5dac2
Thiago Veras <thiagomachado@cjr.org.br>
2017-05-26 18:06:44 -0300

    Update StructSort.cpp

--- StructSort.cpp
+++ StructSort.cpp
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include <bits/stdc++.h>
+using namespace std;
 typedef struct {
  	char pais[200];
  	int ouro, prata,bronze;
