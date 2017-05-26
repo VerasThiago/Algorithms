8ff7a7debf714e7d8a4d74b2bbd94f7d8d1a1c01
Thiago Veras <thiagomachado@cjr.org.br>
2017-05-26 18:06:11 -0300

    Update StructSort.c

--- StructSort.c
+++ StructSort.c
@@ -5,12 +5,9 @@
 typedef struct {
  	char pais[200];
  	int ouro, prata,bronze;
-
- 	
 }selecao;
 
 int cmp (const void *p1, const void *p2){
-
 	selecao *a = (selecao*)p1;
 	selecao *b = (selecao*)p2;
 
@@ -26,23 +23,15 @@ int cmp (const void *p1, const void *p2){
 		else if(a->prata < b->prata)
 			return -1;
 		else{
-
 			if ( a->bronze > b->bronze)
 				return 1;
-
 			else if(a->bronze < b->bronze)
 				return -1;
-
 			else
 				return strcmp(b->pais,a->pais);
 		}
 	}
 }
-
-
-	
-
- 
 int main (){
 	int n, i;
 		scanf("%d", &n);
@@ -52,8 +41,5 @@ int main (){
 				}
 				qsort(v,n,sizeof(selecao),cmp);
 				for( i = n-1 ; i >= 0 ; i--)
-				printf("%s %d %d %d\n", v[i].pais, v[i].ouro, v[i].prata, v[i].bronze);
-	
-       
-    return 0;                  
+				printf("%s %d %d %d\n", v[i].pais, v[i].ouro, v[i].prata, v[i].bronze);          
 }
