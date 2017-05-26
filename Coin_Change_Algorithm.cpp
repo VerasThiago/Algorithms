da30822773543e3f6039ae9cea98a610423b0208
Thiago Veras <thiagomachado@cjr.org.br>
2017-05-26 18:00:14 -0300

    Update

--- Coin_Change_Algorithm.cpp
+++ Coin_Change_Algorithm.cpp
@@ -1,11 +1,11 @@
-
-
-vector<int> ans(n+1);
-ans[0] = 0;
-        for(int i = 1 ; i <= m ; i++)
-            for(int j = 0 ; j < n ; j++)
-                if(v[j] <= i){
-                   
-                    ans[i] = min(ans[i], (1 + ans[i-v[j]]));
-                }
-        
+int coin(int troco, vector<int> moedas){
+	vector<int> ans(troco+1,1e9+7);
+	ans[0] = 0;
+	for(int i = 1 ; i <= troco; i++){
+		for(int j = 0 ; j < moedas.size(); j++){
+			if(i < moedas[j]) break;
+			ans[i] = min(ans[i],1+ans[i - moedas[j]]);
+		}
+	}
+	return ans[troco];
+}
