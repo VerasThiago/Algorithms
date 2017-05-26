e6bc305b4299cfc08a718282335495a6a237e938
Thiago Veras <thiagomachado@cjr.org.br>
2017-05-26 17:53:41 -0300

    Update aux.cpp

--- Data Structures/aux.cpp
+++ Data Structures/aux.cpp
@@ -1,93 +1,44 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-using namespace std/
-
-#define ii pair <int,int>
-map<int,int> v1,v2,v3;
-
-int busca(int x){
-    if(x == 1 and v1.size() == 0 ) return -1;
-    if(x == 2 and v2.size() == 0 ) return -1;
-    if(x == 3 and v3.size() == 0 ) return -1;
-    map<int,int> ::interator it;
-    if(x == 1){
-        it=v1.begin();
-        int aux = it->first;
-        int prox = it->second;
-        v1.erase(it);
-        if(prox != x){
-            if(prox == 2){
-                it = v2.find(aux);
-                v2.erase(it);
-            }
-            else{
-                it = v3.find(aux);
-                v3.erase(it);
-            }
-        }
-    }
-    else if(x == 2){
-        it=v2.begin();
-        int aux = it->first;
-        int prox = it->second;
-        v2.erase(it);
-        if(prox != x){
-            if(prox == 1){
-                it = v1.find(aux);
-                v1.erase(it);
-            }
-            else{
-                it = v3.find(aux);
-                v3.erase(it);
-            }
-        }
-    }
-    else {
-        it=v3.begin();
-        int aux = it->first;
-        int prox = it->second;
-        v3.erase(it);
-        if(prox != x){
-            if(prox == 2){
-                it = v2.find(aux);
-                v2.erase(it);
-            }
-            else{
-                it = v1.find(aux);
-                v1.erase(it);
-            }
-        }
-    }
-    return aux;
+bool ok(double a , double b){
+    return fabs(a-b) < 0.0001;
 }
 
-int main(){
-    int n, x;
-    cin >> n;
-    vector<int> preco(n);
-    vector<int> num(n);
-    vector<int> qnt(4);
-    for(int i = 0 ; i < n ; i++) cin >> preco[i];
-    for(int i = 0 ; i < n ; i++) cin >> num[i];
-    for(int i = 0 ; i < n ; i++){
-        int aux = num[i];
-        cin >> x;
-        qnt[x]aux++;
-        if(aux != x) qnt[aux]++;
-        int teste = min(aux,x);
-        if(teste == 1)
-            v1[preco[i]] = max(aux,x);
-        else if (teste == 2)
-            v2[preco[i]] = max(aux,x);
-        else
-            v3[preco[i]] = max(aux,x);
-    }    
-    int cliente;
-    cin >> cliente;
-    for(int i = 0 ; i < cliente ; i++){
-        cin >> x;
-       cout << busca(x) << " ";        
-    }
-    cout << endl;
-
-}    
+int main() {
+    int n, quero;
+    while(cin >> n >> quero){
+        if(n == 0 and quero == 0) return 0;
+        vector<double> v(n);
+        double L = 0 , R = 0;
+        int total2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            cin >> v[i];
+            total2 += v[i];
+            R = max(R,v[i]);
+        }
+        //cout << "L e R vale " << L << " " << R << endl;       
+        bool penis = true;
+        double mid;
+        if(total2 == quero)
+            cout << ":D" << endl;
+        else{
+            for(int j = 0 ; j <= 100 ; j++){
+                double total = 0;
+                mid = (L+R)/2;
+                for(int i = 0 ; i < n ; i++) total += max(0.0,v[i]-mid );
+                if(ok(total,quero)){
+                    penis = false;
+                    printf("%.4lf\n",mid);
+                    break;
+                }
+                else if(total > quero)
+                    L = mid;
+                else
+                    R = mid;
+            }
+            if(penis)
+                cout << "-.-" << endl;
+        }
+    }        
+}
