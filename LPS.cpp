6f857d3b56a8a37b1bd57ac12724ef270691e709
Thiago Veras <thiagomachado@cjr.org.br>
2017-06-02 18:47:28 -0300

    Update LPS.cpp

--- LPS.cpp
+++ LPS.cpp
@@ -1,24 +1,29 @@
-Longest Palindromic Subsequence
+#include <bits/stdc++.h>
+using namespace std;
+#define ii pair<int,int>
+#define iii pair <int,ii>
+#define MAX 1001
+
+
 int dp[MAX][MAX];
 
-int main(){
-	int x;
-	cin >> x;
-	while(x--){
-		string a;
-		cin >> a;
-		int n = a.size();
-		for(int i = 0 ; i < n ; i++) for(int j = 0 ; j < n ; j++) dp[i][j] = 0;
-		for(int i = 0 ; i < n ; i++) dp[i][i] = 1;
-		for(int i = 0 ; i < n-1 ; i++) dp[i][i+1] = a[i] == a[i+1] ? 2:1;			
-		for(int i = 2 ; i < n ; i++){
-			for(int j = 0 ; j < n-i ; j++){
-				if(a[j] == a[j+i])dp[j][j+i] = 2+dp[j+1][j+i-1];
-				
-				else dp[j][j+i] = max(dp[j+1][j+i],dp[j][j+i-1]);
-			}
-		}
-		cout << dp[0][n-1] << endl;
+int main(){	
+	
+	string a;
+	cin >> a;
+	int n = a.size();
+	
+	for(int i = 0 ; i < n ; i++){
+		dp[i][i] = 1;
+		if(i < n-1) dp[i][i+1] = a[i] == a[i+1] ? 2:1;	
 	}
-
+	
+	for(int i = 2 ; i < n ; i++){
+		for(int j = 0 ; j < n-i ; j++){
+			if(a[j] == a[j+i])dp[j][j+i] = 2+dp[j+1][j+i-1];
+			
+			else dp[j][j+i] = max(dp[j+1][j+i],dp[j][j+i-1]);
+		}
+	}
+	cout << dp[0][n-1] << endl;
 }		
