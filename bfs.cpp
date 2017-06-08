f5b38b5f87a072987f166d9967b1aa5db9f4aa61
Thiago Veras <thiagomachado@cjr.org.br>
2017-06-08 15:22:35 -0300

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,19 @@
+void bfs(int u){
+	queue<int> q;
+	dist[u] = 0;
+	q.push(u);
+	vector<bool> visit(N,false);
+	visit[u] = true;
+	while(!q.empty()){
+		int u = q.front();
+		q.pop();
+		for(int i = 0 ; i < grafo[u].size(); ++i){
+			int v = grafo[u][i];
+			if(!visit[v]){
+				q.push(v);
+				dist[v] = distA[u]+1; 
+				visit[v] = true;
+			}
+		}
+	}
+}
