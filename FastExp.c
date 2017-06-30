7fc34e69e6332cfefcb297366f13f4305a363405
Thiago Veras <thiagomachado@cjr.org.br>
2017-06-30 15:56:58 -0300

    Update FastExp.c

--- FastExp.c
+++ FastExp.c
@@ -1,3 +1,9 @@
+
+2 Types:
+
+
+1-
+
 long long pot(long long x, long long n, long long mod){
 	long long ans = 1;
 	while(n){
@@ -8,4 +14,13 @@ long long pot(long long x, long long n, long long mod){
 	return ans;
 }
 
+
+2- O(Log(n))
+
+int sq(int x){return x*x;}
+ll pot(ll b, ll n){
+	if(n == 0) return 1;
+	if(n%2 == 1) return b * pot(b,n-1);
+	else return sq(pot(b,n/2));
+}
 
