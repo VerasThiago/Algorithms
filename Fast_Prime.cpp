ce39212f13e5b0112a300ea8c713636852a3ff8b
VerasUnB <thiago.machado@cjr.org.br>
2017-09-06 11:56:08 -0300

    New Prime algorithm

--- Fast_Prime.cpp
+++ Fast_Prime.cpp
@@ -1,15 +1,23 @@
-int isPrime(long long int n){
+bool isPrime(long long int n){
     int i;
 
-    if (n==2)
-        return 1;
+    if(n==2)
+        return true;
 
-    if (n%2==0)
-        return 0;
+    if(n%2==0)
+        return false;
 
-    for (i=3;i<=sqrt(n);i+=2)
-        if (n%i==0)
-            return 0;
+    for(i=3;i<=sqrt(n);i+=2)
+        if(n%i==0)
+            return false;
 
-    return 1;
+    return true;
 }
+
+// Eratóstenes algorithm
+
+	vector<bool> prime(N, true);
+	for(int i = 2; i <= sqrt(N); ++i){
+		if(!prime[i]) continue;
+		for(int k = i+i; k <= N; k += i) prime[k] = false;
+	} 
