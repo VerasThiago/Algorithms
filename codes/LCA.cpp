e7c36122157c7353ec8ba9fcc166d6a44b9a1690
VerasThiago <thiago.veras.machado@gmail.com>
2020-11-06 01:08:41 -0300

    Remove Manacher && Suffix Array | Add MST | Refactor LCA

--- codes/LCA.cpp
+++ codes/LCA.cpp
@@ -14,87 +14,104 @@
 
 using namespace std;
  
-vector< int > subTreeSize;
- 
-int buildSize(int u, vector< bool > &visited, vector< vector < int > > &graph){
-    visited[u] = true;
-    for(auto v : graph[u]) if(!visited[v]) subTreeSize[u] += buildSize(v, visited, graph);
-    return subTreeSize[u];
-}
- 
+struct Edge {
+    int u, v, weight;
+    Edge(int uu, int vv, int ww): u(uu), v(vv), weight(ww) {};
+};
  
 class LCA{
- 
+    
 public:
- 
+    
     int n, logN;
     
     bool builded;
- 
+    
     vector< vector< int > > dp;
-    vector< vector< int > > graph;
-    vector< bool > visited;
+    vector< vector< pair<int,int> > > graph;
     vector< int > level;
- 
-    void populateDP(int u, int parent){
-        if(~parent) level[u] = level[parent] + 1;
-        visited[u] = true;
-        dp[u][0] = parent;
-        for(auto v : graph[u]) if(!visited[v]) populateDP(v, u);
-        
+    vector< vector< int >  > maxEdge;
+    
+    void populateDP(int u, int nodeParent){
+        if(~nodeParent) level[u] = level[nodeParent] + 1;
+        dp[u][0] = nodeParent;
+        maxEdge[u][0] = edgeCost[{u, nodeParent}];
+        for(auto v : graph[u]) if(v.first != nodeParent) populateDP(v.first, u);
     }
- 
-    int walk(int u, int d){
+    
+    pair<int,int> walk(int u, int d){
+        int ans = 0;
         while(d){
             int jump = (int)log2(d);
+            ans = max(ans, maxEdge[u][jump]);
             u = dp[u][jump];
             d -= (1 << jump);
         }
-        return u;
+        return {u, ans};
     }
- 
+    
     void build(){
         builded = true;
-        for(int i = 1; i <= n; i++) visited[i] = false;
         populateDP(1, -1);
-        for(int col = 1; col < logN; col++)
-            for(int i = 2; i <= n; i++)
+        for(int col = 1; col < logN; col++){
+            for(int i = 2; i <= n; i++){
                 dp[i][col] = dp[i][col - 1] == -1 ? -1:dp[dp[i][col - 1]][col - 1];
+                maxEdge[i][col] = dp[i][col - 1] == -1 ? -1:max(maxEdge[i][col - 1], maxEdge[dp[i][col - 1]][col - 1]);
+            }
+        }
     }
- 
+    
     LCA(int _n){
         n = _n;
         builded = false;
         logN = (int)log2(n * 2);
         level = vector< int >(n * 2, 0);
-        graph = vector< vector< int > >(n * 2);
-        visited = vector< bool >(n * 2, false);
+        graph = vector< vector< pair<int, int> > >(n * 2);
         dp = vector< vector< int > >(n * 2, vector< int >(logN * 2, -1));
+        maxEdge =  vector< vector< int > >(n * 2, vector< int >(logN * 2, -1));
     }
- 
- 
-    void insert(int x, int y){
-        graph[x].push_back(y);
-        graph[y].push_back(x);
+
+
+    void insert(Edge e){
+        graph[e.u].push_back({e.v, e.weight});
+        graph[e.v].push_back({e.u, e.weight});
     }
- 
+    
     int getLCA(int u, int v){
         if(!builded) build();
         int du = level[u], dv = level[v];
         if(du < dv) return getLCA(v, u);
- 
+    
         int diff = du - dv;
-        u = walk(u, diff);
+        u = walk(u, diff).first;
         
         if(u == v) return u;
- 
-        for(int i = logN; i >= 0; i--){
-            if(dp[u][i] != -1 and dp[u][i] != dp[v][i]) u = dp[u][i], v = dp[v][i];
-        }
+    
+        for(int i = logN; i >= 0; i--) if(dp[u][i] != -1 and dp[u][i] != dp[v][i]) u = dp[u][i], v = dp[v][i];
+        
         return dp[u][0];
     }
- 
-    int dist(int u, int v){
+
+    int getMax(int u, int v){
+        if(!builded) build();
+
+        int du = level[u], dv = level[v];
+        if(du < dv) return getMax(v, u);
+
+        int ans = 0;
+        int diff = du - dv;
+        tie(u, ans) = walk(u, diff);
+        
+        if(u == v)  return ans;
+
+        for(int i = logN; i >= 0; i--) 
+            if(dp[u][i] != -1 and dp[u][i] != dp[v][i]) 
+                ans = max(ans, maxEdge[u][i]), ans = max(ans, maxEdge[v][i]), u = dp[u][i], v = dp[v][i];
+            
+        return max(ans, max(maxEdge[u][0], maxEdge[v][0]));
+    }
+    
+    int dist(int u, int v){ // Only if edge = 1
         return level[u] + level[v] - 2 * level[getLCA(u, v)];
     }
 };
@@ -103,19 +120,15 @@ void solve(){
     int n;
     scanf("%d", &n);
  
-    subTreeSize = vector<int>(2 * n, 1);
-    
     LCA l(n);
  
     for(int i = 1; i < n; i++){
-        int x, y;
-        scanf("%d %d", &x, &y);
-        // build graph
-        l.insert(x,y); 
+        int a, b, w;
+        scanf("%d %d %d", &a, &b, &w);
+        l.insert(Edge(a, b, w));
     }
 
-    // Populate subtree size
-    buildSize(1, l.visited, l.graph);
     auto lca = l.getLCA(a, b);
-    auto dist = l.dist(a, b)
+    auto dist = l.dist(a, b);
+    auto maxEdge = l.getMax(a, b);
 }
---
+++ codes/MST.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define N 200005
+using namespace std;
+
+struct Edge {
+    int u, v, weight;
+    Edge(int uu, int vv, int ww): u(uu), v(vv), weight(ww) {};
+};
+
+int p[N];
+int s[N];
+
+int find(int a) {
+    if (p[a] == a) return a;
+    return p[a] = find(p[a]);
+}
+ 
+void join(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a != b) {
+        if (s[a] < s[b]) swap(a, b);
+        p[b] = a;
+        s[a] += s[b];
+    }
+}
+
+vector<Edge> mst(vector<Edge> edges, int n){
+
+    vector<Edge> result;
+    n++, cost = 0;
+
+    for(int i = 0; i < N; i++)p[i] = i, s[i] = 1;
+
+    sort(edges.begin(), edges.end(), [](const Edge &A, const Edge &B  ){
+        return A.weight < B.weight;
+    });
+
+    for (Edge e : edges) {
+        if (find(e.u) != find(e.v)) {
+            cost += e.weight;
+            result.push_back(e);
+            table[{e.u, e.v}] = true;
+            join(e.u, e.v);
+        }
+    }
+    
+    return result;
+}
+
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    vector<Edge> edges;
+
+    for(int i = 0; i < m; i++){
+        int a, b, w;
+        scanf("%d %d %d", &a, &b, &w);
+        edges.push_back(Edge(a, b, w));
+        edgeCost[{a,b}] = w;
+        edgeCost[{b,a}] = w;
+    }
+
+    vector<Edge> result = mst(edges, n);
+}
